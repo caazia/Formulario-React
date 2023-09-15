cc994812cd2d42b17771f5d4257629bd2bf25708
Cassia Carvalho <contato.cassia.carvalho@gmail.com>
2023-09-15 04:05:05 -0300

    attAula0202

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,31 +2,23 @@ import React, { useState } from 'react';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
 import DadosEntrega from './DadosEntrega';
-import { Typography } from '@material-ui/core';
 
 function FormularioCadastro({aoEnviar, validarCPF}) {
     const [etapaAtual, setEtapaAtual] = useState(0);
 
-    function proximo() {
-        setEtapaAtual(etapaAtual+1)
-    }
+    const formulario =[
+        <DadosUsuario aoEnviar={proximo} />,
+        <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF}/>,
+        <DadosEntrega aoEnviar={aoEnviar}/>
+    ];
 
-    function formularioAtual(etapa) {
-        switch (etapa) {
-            case 0:
-                return <DadosUsuario aoEnviar={proximo} />;
-            case 1:
-                return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF}/>;
-            case 2:
-                return <DadosEntrega aoEnviar={aoEnviar}/>;
-            default:
-                return <Typography>Erro ao selecionar formulário</Typography>
-        }
+    function proximo() {
+        setEtapaAtual(etapaAtual + 1)
     }
 
     return (
         <>
-        {formularioAtual(etapaAtual)} 
+        {formulario[etapaAtual]} 
         </>
     )
     }
